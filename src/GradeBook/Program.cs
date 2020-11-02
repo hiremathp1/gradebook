bcf733b4e5590391ba4d116898d0616321895c67
Panchakshari N Hiremath <hiremathp1@udayton.edu>
2020-11-01 20:18:02 -0500

    Complete code of gradebook works in console

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Praveen's Grade Book");
+             book.GradeAdded += OnGradeAdded;
+             book.GradeAdded += OnGradeAdded;
+             book.GradeAdded -= OnGradeAdded;
+           
+
+           
+           while(true)
+           {
+                      Console.WriteLine("Enter the grade or press 'Q' quit");
+                       var input = Console.ReadLine();
+                  if(input == "q")
+                  {
+                     break;
+
+                  }
+
+                  try
+
+                  {
+                       var grade = double.Parse(input);
+                  book.AddGrade(grade);
+                  }
+                  catch(ArgumentException exec )
+                  {
+                      Console.WriteLine(exec.Message);
+
+                  }
+                  catch(FormatException exec)
+                  {
+                      Console.WriteLine(exec.Message);
+
+                  }
+                  finally
+                  {
+                      Console.WriteLine("QIt's an error");
+                  }
+
+           }
+          //  book.AddGrade(85.6);
+           // book.AddGrade(90.5);
+             //book.AddGrade(77.3);
+            //book.GetStatistics();
+
+        var stats = book.GetStatistics();
+        Console.WriteLine(Book.CATEGORY);
+        Console.WriteLine($"For the book named {book.Name}");
+        Console.WriteLine($" The Avarage grade is {stats.Average:N1}, Highest Grade is {stats.High:N1}, Lowest grade {stats.Low:N1}, The Grade is {stats.Letter} ");
+
+          
+     }
+
+     static void OnGradeAdded(object sender, EventArgs e)
+     {
+                  Console.WriteLine("A grade was added");
+     }
+    }
+}
+
---
+++ src/GradeBook/Statistics.cs
@@ -0,0 +1,18 @@
+
+
+
+namespace GradeBook
+{
+
+
+  public class Statistics{
+    
+  public double Average;
+  public double High;
+  public double Low;
+
+  public char Letter;
+  
+
+  }
+}
